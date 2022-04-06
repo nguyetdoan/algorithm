0d10ac8bc195adc5b3c483859fdf74553f25a602
nguyetdoan <minhnguyetdoan057@gmail.com>
2022-04-06 22:38:02 +0700

    Merge sort

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -32,3 +32,28 @@ function mergeSort(arr) {
 // Both best and worst
 // Time complexity O(n log n)
 // Space complexity O(n log n)
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+// To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+var merge2 = function(nums1, m, nums2, n) {
+    let len = m + n - 1;
+    m--;
+    n--;
+    while (n >= 0) {
+        if (nums1[m] > nums2[n] && m >= 0) {
+            nums1[len] = nums1[m];
+            m--;
+        } else {
+            nums1[len] = nums2[n];
+            n--;
+        }
+        len--;
+    }
+    
+    return nums1
+};
